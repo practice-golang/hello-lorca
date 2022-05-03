b4ca716d216a511934e3448c1e1f7bbbde3e8f02
Practice Golang <practice.golang@gmail.com>
2022-05-03 15:24:14 +0900

    Remove lorca pkg

--- main.go
+++ main.go
@@ -41,13 +41,76 @@ func main() {
 	// Create UI with basic HTML passed via data URI
 	ui, err := lorca.New("data:text/html,"+url.PathEscape(`
 	<html>
-		<head><title>Hello</title></head>
-		<body>
-			<h1>Hello, world!</h1>
-			<a href="data:application/xml;charset=utf-8,your code here" download="filename.html">Save</a>
-			<input type="file">
-			<button onclick="save()">Save</button>
-		</body>
+
+	<head>
+		<title>Hello</title>
+	</head>
+
+	<body>
+		<h1>Hello, world!</h1>
+		<a href="data:application/xml;charset=utf-8,your code here" download="filename.html">Save</a>
+		<input type="file">
+		<button onclick="save()">Save</button>
+
+		<button onclick="openFileBrowser()">Open file picker</button>
+		<button onclick="save()">Save</button>
+		<button onclick="saveAS()">Save as</button>
+
+		<div id="text-area">This is default text</div>
+	</body>
+	<script>
+		let fileHandle
+
+		async function openFileBrowser() {
+			[fileHandle] = await window.showOpenFilePicker()
+			const f = await fileHandle.getFile()
+			const contents = await f.text()
+
+			document.getElementById('text-area').innerText = contents
+		}
+
+		async function save() {
+			if (fileHandle == undefined) {
+				alert("File is not opened")
+				return false
+			}
+
+			const writer = await fileHandle.createWritable()
+			await writer.write(document.getElementById("text-area").innerText)
+			await writer.close()
+		}
+
+		async function saveAS() {
+			const handle = await getNewFileHandle()
+			const contents = document.getElementById("text-area").innerText
+			await writeFile(handle, contents)
+		}
+
+		async function writeFile(handle, contents) {
+
+			const writable = await handle.createWritable()
+			await writable.write(contents)
+			await writable.close()
+		}
+
+		async function getNewFileHandle() {
+			const options = {
+				types: [{
+					description: "Text Files",
+					accept: { "text/plain": [".txt"] },
+				}],
+			};
+			const handle = await window.showSaveFilePicker(options);
+			return handle;
+		}
+
+		async function writeFile(fileHandle, contents) {
+			const writable = await fileHandle.createWritable()
+			await writable.write(contents)
+			await writable.close()
+		}
+	</script>
+
 	</html>
 	`), profilePath, 1024, 768)
 	// `), "", 480, 320)
