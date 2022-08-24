9d01333807bd7f6f4c56946a7e2b537780c496be
Practice Golang <practice.golang@gmail.com>
2022-08-25 05:07:32 +0900

    Kick out popup bubbling

--- main.go
+++ main.go
@@ -31,8 +31,36 @@ func initLorca() {
 	cwd, _ := os.Getwd()
 	profilePath := cwd + `\profile`
 
-	args := []string{"--ash-force-desktop"}
-	// args := []string{}
+	lorca.DefaultChromeArgs = []string{
+		"--disable-background-networking",
+		"--disable-background-timer-throttling",
+		"--disable-backgrounding-occluded-windows",
+		"--disable-breakpad",
+		"--disable-client-side-phishing-detection",
+		"--disable-default-apps",
+		"--disable-dev-shm-usage",
+		"--disable-infobars",
+		"--disable-extensions",
+		"--disable-features=site-per-process",
+		"--disable-hang-monitor",
+		"--disable-ipc-flooding-protection",
+		"--disable-popup-blocking",
+		"--disable-prompt-on-repost",
+		"--disable-renderer-backgrounding",
+		"--disable-sync",
+		"--disable-translate",
+		// "--disable-windows10-custom-titlebar",
+		"--metrics-recording-only",
+		// "--no-first-run",
+		"--no-default-browser-check",
+		"--safebrowsing-disable-auto-update",
+		// "--enable-automation",
+		"--password-store=basic",
+		"--use-mock-keychain",
+	}
+
+	// args := []string{"--ash-force-desktop", "--force-app-mode"}
+	args := []string{}
 
 	ui, err := lorca.New("http://localhost:1323", profilePath, 1024, 768, args...)
 	if err != nil {
@@ -41,6 +69,8 @@ func initLorca() {
 	defer ui.Close()
 
 	<-ui.Done()
+
+	os.RemoveAll(cwd + `/profile`)
 }
 
 func main() {
